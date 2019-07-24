88412987ca8e84646e6762882da379892b22613e
Enomae <manuelromem@gmail.com>
2019-07-25 02:09:12 +1000

    Add Time to Event, time and date pickers

--- app/src/main/java/com/example/planner/activity/AddEnventActivity.java
+++ app/src/main/java/com/example/planner/activity/AddEnventActivity.java
@@ -1,9 +1,13 @@
 package com.example.planner.activity;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.planner.NetworkService;
 import com.example.planner.R;
@@ -11,6 +15,8 @@ import com.example.planner.dao.Event;
 import com.example.planner.dao.EventPattern;
 import com.example.planner.dto.EventPatternResponse;
 import com.example.planner.dto.EventResponse;
+
+import java.util.GregorianCalendar;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,17 +27,62 @@ public class AddEnventActivity extends AppCompatActivity {
     private NetworkService networkService = NetworkService.getInstance();
     private String userToken = "serega_mem";
 
+    private TextView eventName;
+    private TextView eventDetails;
+    private TextView eventStatus;
+
+    private DatePicker eventStartDatePicker;
+    private DatePicker eventEndDatePicker;
+    private TimePicker eventStartTimePicker;
+    private TimePicker eventEndTimePicker;
+
+    private GregorianCalendar gregorianCalendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_envent);
+
+        eventStartTimePicker = findViewById(R.id.eventStartTimePicker);
+        eventEndTimePicker = findViewById(R.id.eventEndTimePicker);
+        eventStartTimePicker.setIs24HourView(true);
+        eventEndTimePicker.setIs24HourView(true);
     }
 
     public void addEvent(View view) {
-        TextView eventName = findViewById(R.id.eventName);
-        TextView eventDetails = findViewById(R.id.eventDetails);
-        TextView eventLocation = findViewById(R.id.eventLocation);
-        TextView eventStatus = findViewById(R.id.eventStatus);
+
+        eventName = findViewById(R.id.eventName);
+        eventDetails = findViewById(R.id.eventDetails);
+        eventStatus = findViewById(R.id.eventStatus);
+
+        eventStartDatePicker = findViewById(R.id.eventStartDatePicker);
+        eventEndDatePicker = findViewById(R.id.eventEndDatePicker);
+        eventStartTimePicker = findViewById(R.id.eventStartTimePicker);
+        eventEndTimePicker = findViewById(R.id.eventEndTimePicker);
+
+        gregorianCalendar = new GregorianCalendar();
+
+        Long startAt;
+        Long endAt;
+        Long duration;
+
+        gregorianCalendar.set(
+                eventStartDatePicker.getYear(),
+                eventStartDatePicker.getMonth(),
+                eventStartDatePicker.getDayOfMonth(),
+                eventStartTimePicker.getHour(),
+                eventStartTimePicker.getMinute());
+
+        startAt = gregorianCalendar.getTimeInMillis();
+
+        gregorianCalendar.set(
+                eventEndDatePicker.getYear(),
+                eventEndDatePicker.getMonth(),
+                eventEndDatePicker.getDayOfMonth(),
+                eventEndTimePicker.getHour(),
+                eventEndTimePicker.getMinute());
+
+        endAt = gregorianCalendar.getTimeInMillis();
 
         Event event = new Event(
                 eventDetails.getText().toString(),
@@ -39,11 +90,13 @@ public class AddEnventActivity extends AppCompatActivity {
                 eventName.getText().toString(),
                 eventStatus.getText().toString());
 
-        EventPattern eventPattern = new EventPattern(7200000L,
-                1563960257406L + 7200000L,
+        duration = endAt - startAt;
+
+        EventPattern eventPattern = new EventPattern(duration,
+                endAt,
                 null,
                 null,
-                1563960257406L,
+                startAt,
                 "GMT");
 
 
@@ -80,5 +133,6 @@ public class AddEnventActivity extends AppCompatActivity {
         });
 
     }
+
 
 }
---
+++ app/src/main/java/com/example/planner/activity/SelectTime.java
@@ -0,0 +1,38 @@
+package com.example.planner.activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.example.planner.R;
+
+
+public class SelectTime extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_select_time, container, false);
+        TimePicker timePicker = view.findViewById(R.id.timePicker);
+
+        return view;
+    }
+
+
+}
